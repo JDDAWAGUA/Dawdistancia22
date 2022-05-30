d2cc20a3f58051054da01c031495e22441c9dc5a
JDDAWAGUA <106261245+JDDAWAGUA@users.noreply.github.com>
2022-05-30 11:10:44 +0200

    Add files via upload

---
+++ practica_alcantara.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Ejemplo, phpDocumentor Jesus Daniel Alcantara Montes
+ * IES Aguadulce curso 21/22 
+ * 
+ * Este archivo muestra el uso de la etiqueta @name 
+ * @author Jesus Daniel Alcantara Montes
+ * @version 1.0
+ */
+ 
+/**
+ * Función suma de dos números
+ * @param int num_a
+ * @param int num_b
+ * @return int
+ */ 
+function suma($num_a, $num_b){
+$resultado = $num_a + $num_b;
+return $resultado;
+}
+ /**
+ * Función resta de dos números
+ *@param int num_1
+ *@param int num_2
+ *@return $resultado_resta
+ *{@param $resultado_resta} Indica la funcion resta entre dos numeros, puede dar negativos
+ */
+ 
+function resta($num_a, $num_b){
+
+$resultado_resta = $num_a - $num_b;
+return $resultado_resta;
+}
+$suma =suma(3, 7);
+$resta = resta(4, 6);
+echo $suma;
+echo $resta;
+?>
